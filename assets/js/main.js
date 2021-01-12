22c41f7abc92be23ae0c4b7ff9673edc62a4a2c9
AnJaWr <october3@o2.pl>
2021-01-12 14:16:56 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  var $one = $('#quote_text'),
+    $two = $('#about_text'),
+    $three = $('#contact_text');
+
+  $two.hide();
+  $three.hide();
+
+  $('#heading').click(function () {
+    var $in, $out1, $out2;
+    if ($one.is(':visible')) {
+      ($in = $two), ($out1 = $one), ($out2 = $three);
+    } else {
+      ($in = $one), ($out1 = $two), ($out2 = $three);
+    }
+    $($out1, $out2).fadeOut('slow', function () {
+      $in.fadeIn();
+    });
+  });
+
+  $('#about-btn').click(function () {
+    var $in, $out1, $out2;
+    if ($one.is(':visible')) {
+      ($in = $two), ($out1 = $one), ($out2 = $three);
+    } else if ($three.is(':visible')) {
+      ($in = $two), ($out1 = $one), ($out2 = $three);
+    }
+    $($out1, $out2).fadeOut('slow', function () {
+      $in.fadeIn();
+    });
+
+    if ($two.is(':visible')) {
+      $one.hide();
+      $three.hide();
+    }
+  });
+
+  $('#home-btn').click(function () {
+    var $in, $out1, $out2;
+    if ($two.is(':visible')) {
+      ($in = $one), ($out1 = $two), ($out2 = $three);
+    } else if ($three.is(':visible')) {
+      ($in = $one), ($out1 = $two), ($out2 = $three);
+    }
+    $($out1, $out2).fadeOut('slow', function () {
+      $in.fadeIn();
+    });
+
+    if ($one.is(':visible')) {
+      $two.hide();
+      $three.hide();
+    }
+  });
+
+  $('#contact-btn').click(function () {
+    var $in, $out1, $out2;
+    if ($one.is(':visible')) {
+      ($in = $three), ($out1 = $two), ($out2 = $one);
+    } else if ($two.is(':visible')) {
+      ($in = $three), ($out1 = $two), ($out2 = $one);
+    } else if ($three.is(':visible')) {
+      ($in = $three), ($out1 = $two), ($out2 = $one);
+    }
+    $($out1, $out2).fadeOut('slow', function () {
+      $in.fadeIn('slow');
+    });
+
+    if ($three.is(':visible')) {
+      $one.hide();
+      $two.hide();
+    }
+  });
+});
